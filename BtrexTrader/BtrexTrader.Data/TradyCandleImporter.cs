347a104203245ce4770c578567ad5ce7f521ca53
Mike Holberger <mholberger@gmail.com>
2017-10-21 00:14:32 -0600

    .

--- BtrexTrader/BtrexTrader.Data/TradyCandleImporter.cs
+++ BtrexTrader/BtrexTrader.Data/TradyCandleImporter.cs
@@ -15,8 +15,7 @@ namespace BtrexTrader.Data
         public async Task<IReadOnlyList<Candle>> ImportAsync(string symbol, DateTime? startTime = null, DateTime? endTime = null, PeriodOption period = PeriodOption.Daily, CancellationToken token = default(CancellationToken))
         {
             //TODO:COMPLETE IMPORTER
-            //IMport Candles from Data.Candle5m, 
-            //If thats not enough, trim the fuzz and get more data from sqlite
+            //Import Candles from Data.Candle5m, get more data from sqlite
             //Assemble/Return IReadOnlyList in correct order.
             if (endTime == null)
                 endTime = DateTime.UtcNow;
